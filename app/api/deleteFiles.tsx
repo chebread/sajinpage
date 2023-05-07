d72a2362beba9823db6a7deb644661ba03b3f5cc
Cha Haneum <developer_haneum_11@icloud.com>
2023-05-07 19:28:29 +0900

    🔥 remove firebase codes
    Firebase 관련 코드 삭제

--- app/api/deleteFiles.tsx
+++ app/api/deleteFiles.tsx
@@ -1,19 +1,18 @@
-import { db, doc, deleteDoc, deleteObject } from 'components/firebase';
-import getFileRef from './getFileRef';
+// import getFileRef from './getFileRef';
 
 const deleteFile = ({ fileId }) => {
-  getFileRef({ fileId })
-    .then(fileRef => {
-      Promise.all([
-        // storage에 있는 파일 삭제
-        deleteObject(fileRef),
-        // db에 있는 파일 삭제
-        deleteDoc(doc(db, 'images', fileId)),
-      ]);
-    })
-    .catch(err => {
-      console.log(err);
-    }); // storage의 그 자체의 파일 참조 아이디
+  // getFileRef({ fileId })
+  //   .then(fileRef => {
+  //     Promise.all([
+  //       // storage에 있는 파일 삭제
+  //       deleteObject(fileRef),
+  //       // db에 있는 파일 삭제
+  //       deleteDoc(doc(db, 'images', fileId)),
+  //     ]);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   }); // storage의 그 자체의 파일 참조 아이디
 };
 
 export default deleteFile;
--- app/api/index.tsx
+++ app/api/index.tsx
@@ -2,14 +2,6 @@ import uploadFiles from './uploadFiles';
 import deleteFile from './deleteFiles';
 import loadFiles from './loadFiles';
 import cancelUploadFiles from './cancelUploadFiles';
-import getFileRef from './getFileRef';
 import getFileUrl from './getFileUrl';
 
-export {
-  uploadFiles,
-  deleteFile,
-  loadFiles,
-  cancelUploadFiles,
-  getFileRef,
-  getFileUrl,
-};
+export { uploadFiles, deleteFile, loadFiles, cancelUploadFiles, getFileUrl };
