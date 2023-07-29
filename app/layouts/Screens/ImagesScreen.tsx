aa31bd825308a9f3d62234c4ef5c4608ce82bed1
Cha Haneum <developer_haneum_11@icloud.com>
2023-07-30 02:18:12 +0900

    ⚡️ add viewer image radius feature

--- app/layouts/Screens/ImagesScreen.tsx
+++ app/layouts/Screens/ImagesScreen.tsx
@@ -1,6 +1,6 @@
 import { errorAtom } from 'atoms/errorAtom';
 import { useAtom } from 'jotai';
-import { centerAlign } from 'layouts/properties';
+import { centerAlign, desktopVp } from 'layouts/properties';
 import styled from 'styled-components';
 
 // (0): border-radius 기능 추가하기
@@ -10,18 +10,20 @@ const ImagesScreen = ({ src }) => {
 
   return (
     <Container>
-      <ImageWrapper>
-        <Image
-          src={src}
-          onError={() => {
-            // Uxpected error tracking (avif browser 지원안할때, file url 손상시, ...)
-            onError({
-              code: 405,
-              message: 'ImagesViewer에서 image를 불러오는 중에 에러가 발생함',
-            });
-          }}
-        />
-      </ImageWrapper>
+      <Wrapper>
+        <ImageWrapper>
+          <Image
+            src={src}
+            onError={() => {
+              // Uxpected error tracking (avif browser 지원안할때, file url 손상시, ...)
+              onError({
+                code: 405,
+                message: 'ImagesViewer에서 image를 불러오는 중에 에러가 발생함',
+              });
+            }}
+          />
+        </ImageWrapper>
+      </Wrapper>
     </Container>
   );
 };
@@ -29,15 +31,19 @@ const Container = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
-  ${centerAlign}/* background-color: seagreen; */
+  ${centerAlign}
 `;
-const ImageWrapper = styled.div`
+const Wrapper = styled.div`
   position: absolute;
   height: 100%;
   width: 100%;
   display: flex;
   flex-direction: column;
   ${centerAlign}
+`;
+const ImageWrapper = styled.div`
+  max-height: 100%;
+  max-width: 100%;
 `;
 const Image = styled.img`
   display: block;
@@ -46,5 +52,9 @@ const Image = styled.img`
   margin: auto;
   object-fit: contain;
   object-position: center;
+  border-radius: 0.5rem;
+  @media (${desktopVp}) {
+    border-radius: 1rem;
+  }
 `;
 export default ImagesScreen;
